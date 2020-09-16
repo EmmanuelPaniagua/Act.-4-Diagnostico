bafbf57bbc63d965f93a6216f11f927a4a920333
Emmanuel Paniagua <emmanuel.paniagua4671@alumnos.udg.mx>
2020-09-16 01:46:20 -0500

    Cree la función capturar enteros, solicitamos 5 enteros al usuario, los imprimimos junto a su suma y promedio

--- Diagnostico.c
+++ Diagnostico.c
@@ -1,1 +1,56 @@
 #include <stdio.h>
+
+void capturar_enteros()
+{
+	int enteros[5];
+	float promedio;
+	int suma = 0;
+	int i;
+	
+	printf("\nIngresa los enteros\n");
+	for ( i = 0; i < 5 ; i++)
+	{
+		scanf("%i", &enteros[i]);
+	}
+	for ( i = 0; i < 5 ; i++)
+	{
+		printf("%i\n", enteros[i]);
+	}
+	for ( i = 0; i < 5 ; i++)
+	{
+		suma = suma + enteros[i];
+	}
+	printf("Suma: %i\n", suma);
+	printf("Promedio: %f\n", suma/5.0);
+}
+
+int main()
+{
+	char op;
+	do
+	{
+		system("cls");
+		printf("MENU\n");
+		printf("1) Capturar enteros\n");
+		printf("2) Mostrar cadena n veces \n");
+		printf("3) Agrega personaje\n");
+		printf("0) Salir\n");
+		scanf("%c", &op);
+		fflush(stdin);
+		switch (op)
+		{
+			case '1':
+				capturar_enteros();
+				system ("pause");
+				break;
+			case '2':
+				break;
+			case '3':
+				break;
+			default:
+				break;
+		}	
+	}while(op != '0');
+	return 0;
+	
+}
